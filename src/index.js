52b6ff6c0be5618147bf96bb1f07f4982f6ac267
kirtanchandak <themarketingkirtan@gmail.com>
2023-02-15 23:22:27 +0530

    react-query setup

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <QueryClientProvider client={new QueryClient()}>
     <App />
-  </React.StrictMode>
+  </QueryClientProvider>
 );
